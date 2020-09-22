a9c36b71c514fd67d1e3ad83f0343631d25a3290
lin <465382251@qq.com>
2020-09-22 11:19:59 +0800

    add header

--- src/Request.php
+++ src/Request.php
@@ -11,66 +11,66 @@ use Lin\Ku\Exceptions\Exception;
 class Request
 {
     protected $key='';
-    
+
     protected $secret='';
-    
+
     protected $host='';
-    
+
     protected $passphrase='';
-    
-    
-    
+
+
+
     protected $nonce='';
-    
+
     protected $signature='';
-    
+
     protected $headers=[];
-    
+
     protected $type='';
-    
+
     protected $path='';
-    
+
     protected $data=[];
-    
+
     protected $options=[];
-    
+
     public function __construct(array $data)
     {
         $this->key=$data['key'] ?? '';
         $this->secret=$data['secret'] ?? '';
         $this->passphrase = $data['passphrase'] ?? '';
         $this->host=$data['host'] ?? '';
-        
+
         $this->options=$data['options'] ?? [];
     }
-    
+
     /**
-     * 认证
+     *
      * */
     protected function auth(){
         $this->nonce();
-        
+
         $this->signature();
-        
+
         $this->headers();
-        
+
         $this->options();
     }
-    
+
     /**
-     * 过期时间
+     *
      * */
     protected function nonce(){
         $this->nonce=floor(microtime(true) * 1000);
     }
-    
+
     /**
-     * 签名
+     *
      * */
     protected function signature(){
         $body='';
         $path=$this->type.$this->path;
-        
+
         if (!empty($this->data)) {
             if($this->type=='GET') {
                 $path.='?'.http_build_query($this->data);
@@ -78,36 +78,34 @@ class Request
                 $body=json_encode($this->data);
             }
         }
-        
+
         $plain = $this->nonce . $path . $body;
         $this->signature = base64_encode(hash_hmac('sha256', $plain, $this->secret, true));
     }
-    
+
     /**
-     * 默认头部信息
+     *
      * */
     protected function headers(){
         $this->headers= [
             'Content-Type' => 'application/json',
-            
+
             'KC-API-KEY'        => $this->key,
             'KC-API-TIMESTAMP'  => $this->nonce,
             'KC-API-PASSPHRASE' => $this->passphrase,
             'KC-API-SIGN'       => $this->signature,
         ];
     }
-    
+
     /**
-     * 请求设置
+     *
      * */
     protected function options(){
-        $this->options=array_merge([
-            'headers'=>$this->headers,
-            //'verify'=>false   //关闭证书认证
-        ],$this->options);
-        
+        if(isset($this->options['headers'])) $this->headers=array_merge($this->headers,$this->options['headers']);
+
+        $this->options['headers']=$this->headers;
         $this->options['timeout'] = $this->options['timeout'] ?? 60;
-        
+
         if(isset($this->options['proxy']) && $this->options['proxy']===true) {
             $this->options['proxy']=[
                 'http'  => 'http://127.0.0.1:12333',
@@ -116,15 +114,15 @@ class Request
             ];
         }
     }
-    
+
     /**
-     * 发送http
+     *
      * */
     protected function send(){
         $client = new \GuzzleHttp\Client();
-        
+
         $url=$this->host.$this->path;
-        
+
         if(!empty($this->data)) {
             if($this->type=='GET') {
                 $url.='?'.http_build_query($this->data);
@@ -132,25 +130,24 @@ class Request
                 $this->options['body']=json_encode($this->data);
             }
         }
-        
+
         $response = $client->request($this->type, $url, $this->options);
-        
+
         return $response->getBody()->getContents();
     }
-    
+
     /*
-     * 执行流程
+     *
      * */
     protected function exec(){
         $this->auth();
-        
-        //可以记录日志
+
         try {
             return json_decode($this->send(),true);
         }catch (RequestException $e){
             if(method_exists($e->getResponse(),'getBody')){
                 $contents=$e->getResponse()->getBody()->getContents();
-                
+
                 $temp=json_decode($contents,true);
                 if(!empty($temp)) {
                     $temp['_method']=$this->type;
@@ -161,10 +158,9 @@ class Request
             }else{
                 $temp['_message']=$e->getMessage();
             }
-            
+
             $temp['_httpcode']=$e->getCode();
-            
-            //TODO  该流程可以记录各种日志
+
             throw new Exception(json_encode($temp));
         }
     }
